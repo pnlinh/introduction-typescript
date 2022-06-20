86b062ce56eeb6fbe3acf2fe39abb8e6683cbf57
Ngoc Linh Pham <pnlinh1207@gmail.com>
2022-06-20 15:40:55 +0700

    object type

--- app.js
+++ app.js
@@ -6,3 +6,8 @@ var num1 = 5;
 var num2 = 2.8;
 var result = add(num1, num2);
 console.log(result);
+var person = {
+    name: 'John Doe',
+    age: '30'
+};
+console.log(person);
--- app.ts
+++ app.ts
@@ -10,3 +10,13 @@ const num2 = 2.8;
 const result = add(num1, num2);
 
 console.log(result);
+
+const person: {
+    name: string;
+    age: number;
+} = {
+    name: 'John Doe',
+    age: 30
+}
+
+console.log(person);
