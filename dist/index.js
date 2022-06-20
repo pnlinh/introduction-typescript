8e1b8fdbeade0aaa80b324a3b18bd52c9bb6f1b7
Ngoc Linh Pham <pnlinh1207@gmail.com>
2022-06-20 16:58:52 +0700

    basic ts

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,84 @@
 "use strict";
-function add(num1, num2) {
-    console.log(typeof num1);
-    return num1 + num2;
-}
-const num1 = 5;
-const num2 = 2.8;
-const result = add(num1, num2);
-console.log(result);
-const person = {
+// Basic Types
+let id = 5;
+let company = 'Foobar';
+let isPublished = false;
+let x = 'Hello';
+let age = 30;
+let ids = [1, 2, 3];
+let arr = [1, true, 'foo', 'bar'];
+// Tuple
+let person = [1, 'foo', true];
+// Tuple array
+let employee = [[1, 'foo'], [2, 'bar']];
+// Union
+let uId = 100;
+// Enum
+var UserState;
+(function (UserState) {
+    UserState[UserState["Active"] = 1] = "Active";
+    UserState[UserState["Deactivate"] = 2] = "Deactivate";
+})(UserState || (UserState = {}));
+console.log(UserState.Active);
+var PostState;
+(function (PostState) {
+    PostState["Draft"] = "draft";
+    PostState["Published"] = "published";
+})(PostState || (PostState = {}));
+console.log(PostState.Published);
+const user = {
+    id: 1,
     name: 'John Doe',
     age: 30
 };
-console.log(person);
+console.log(user);
+// Type Assertion
+let cid = 1;
+// let customerId = <number>cid;
+let customerId = cid;
+// Functions
+function add(x, y) {
+    return x + y;
+}
+console.log(add(1, 2));
+// Void
+function log(message) {
+    console.log(message);
+}
+log('Hi');
+log(123);
+const user1 = {
+    id: 1,
+    name: 'John Doe',
+};
+const p1 = 1;
+const addFunc = (x, y) => x + y;
+const subFunc = (x, y) => x - y;
+console.log(addFunc(1, 2));
+console.log(subFunc(1, 2));
+class Person {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    eat() {
+        return `${this.name} was eating`;
+    }
+}
+const brad = new Person(1, 'John');
+console.log(brad.id, brad.eat());
+class Student extends Person {
+    constructor(id, name, className) {
+        super(id, name);
+        this.className = className;
+    }
+}
+const st = new Student(2, 'TrungX', 'FRT');
+console.log(st, st.eat());
+// Generics
+function getArray(items) {
+    return [].concat(items);
+}
+let numArr = getArray([1, 2, 3]);
+let strArr = getArray(['1', '2', '3']);
+console.log(numArr);
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,137 @@
-function add(num1: number, num2: number) {
-    console.log(typeof num1)
+// Basic Types
+let id: number = 5;
+let company: string = 'Foobar';
+let isPublished: boolean = false;
+let x: any = 'Hello';
+let age: number = 30;
 
-    return num1 + num2;
+let ids: number[] = [1, 2, 3];
+let arr: any = [1, true, 'foo', 'bar'];
+
+// Tuple
+let person: [number, string, boolean] = [1, 'foo', true];
+// Tuple array
+let employee: [number, string][] = [[1, 'foo'], [2, 'bar']];
+
+// Union
+let uId: string | number = 100;
+
+// Enum
+enum UserState {
+    Active = 1,
+    Deactivate = 2
 }
 
-const num1 = 5;
-const num2 = 2.8;
+console.log(UserState.Active)
 
-const result = add(num1, num2);
+enum PostState {
+    Draft = 'draft',
+    Published = 'published',
+}
 
-console.log(result);
+console.log(PostState.Published)
 
-const person: {
-    name: string;
-    age: number;
-} = {
+// Object
+type User = {
+    id: number,
+    name: string,
+    age: number
+};
+
+const user: User = {
+    id: 1,
     name: 'John Doe',
     age: 30
 }
 
-console.log(person);
+console.log(user)
+
+// Type Assertion
+let cid: any = 1;
+// let customerId = <number>cid;
+let customerId = cid as number;
+
+// Functions
+function add(x: number, y: number): number {
+    return x + y;
+}
+
+console.log(add(1, 2));
+
+// Void
+function log(message: string | number) {
+    console.log(message)
+}
+
+log('Hi');
+log(123);
+
+// Interfaces
+interface UserInterface {
+    id: number,
+    name: string,
+    age?: number,
+}
+
+const user1: UserInterface = {
+    id: 1,
+    name: 'John Doe',
+}
+
+type Point = number | string;
+const p1: Point = 1;
+
+interface MathFunc {
+    (x: number, y: number): number
+}
+
+const addFunc: MathFunc = (x: number, y: number): number => x + y;
+const subFunc: MathFunc = (x: number, y: number): number => x - y;
+
+console.log(addFunc(1, 2))
+console.log(subFunc(1, 2))
+
+// Classes
+interface PersonInterface {
+    id: number,
+    name: string,
+    eat(): string
+}
+
+class Person implements PersonInterface {
+    public id: number;
+    name: string;
+
+    constructor(id: number, name: string) {
+        this.id = id;
+        this.name = name;
+    }
+
+    eat() {
+        return `${this.name} was eating`;
+    }
+}
+
+const brad = new Person(1, 'John');
+console.log(brad.id, brad.eat());
+
+class Student extends Person {
+    className: string;
+
+    constructor(id: number, name: string, className: string) {
+        super(id, name);
+        this.className = className
+    }
+}
+
+const st = new Student(2, 'TrungX', 'FRT');
+console.log(st, st.eat());
+
+// Generics
+function getArray<T>(items: T[]): T[] {
+    return [].concat(items);
+}
+
+let numArr = getArray<number>([1, 2, 3]);
+let strArr = getArray<string>(['1', '2', '3']);
+console.log(numArr);
